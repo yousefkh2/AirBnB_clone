53d491ad3f5e9c20103bb0adb3dda16070fd1691
anasyaser <anoos649@gmail.com>
2023-08-10 10:36:03 +0300

    Intialize files storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module: engine
+
+mange saved and loaded files
+"""
+
+
+class FileStorate:
+    """
+    storage objects to file and retrieve them
+    """
+
+    def __init__(self):
+        self.__file_path = ""
+        self.__objects = {}
+
+    
