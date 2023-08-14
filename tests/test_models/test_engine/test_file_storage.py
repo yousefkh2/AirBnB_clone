48f7cd199d0a36bc279c0721efa36186fd7b6119
anasyaser <anoos649@gmail.com>
2023-08-14 12:15:48 +0300

    Add test cases for file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -17,10 +17,38 @@ class TestFileStorage(unittest.TestCase):
     def setUp(self):
         """Intialize file storage instance"""
         self.st = FileStorage()
+        self.bs1 = models.base_model.BaseModel()
+        self.ur1 = models.user.User()
+        self.file_attr = "_FileStorage__file_path"
+        self.obj_attr = "_FileStorage__objects"
+        self.objects_dict = getattr(self.st, self.obj_attr)
+        self.file_obj = getattr(self.st, self.file_attr)
 
+    def test_attributes_types(self):
+        """Test types of attributes"""
+        self.assertIsInstance(self.objects_dict, dict)
+        self.assertIsInstance(self.file_obj, str)
+
+    def test_new(self):
+        """Test adding properly new objects to current saved objects"""
+        self.st.new(self.bs1)
+        self.st.new(self.ur1)
+        self.assertIn(self.bs1.__class__.__name__ + "." + self.bs1.id,
+                        self.objects_dict)
+        self.assertIn(self.ur1.__class__.__name__ + "." + self.ur1.id,
+                        self.objects_dict)
+
+    def test_all(self):
+        """Test all method"""
+        d = self.st.all()
+        self.assertIs(d, getattr(FileStorage, self.obj_attr))
 
     def test_file_existance(self):
         pass
 
     def test_storage_types(self):
         pass
+
+    def tearDown(self):
+        """Remove testing files"""
+        
